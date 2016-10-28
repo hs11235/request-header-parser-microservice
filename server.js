6d7a07593fd6916b130e9e058a0cc1e74d8c5813
Hayden Summers <haydensummers_1994@hotmail.co.uk>
2016-10-28 11:53:40 +0000

    solution

--- server.js
+++ server.js
@@ -3,7 +3,27 @@ var app = express();
 
 app.get('/', function(req, res) {
     
-    res.send('Hello World!');
+    var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+     
+    var software = req.headers['user-agent'];
+    software = software.match(/\((.*)\)/)[1];
+    
+    var language = req.headers['accept-language'];
+    language = language.split(',')[0];
+     
+    //console.log(ip);
+    //console.log(software);
+    //console.log(language);
+    //console.log(req);
+    
+    res.json({
+        'ipaddress': ip,
+        'language': language,
+        'software': software
+    });
 });
 
 app.listen(8080, function() {
